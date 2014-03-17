df57f42fab3079aa728a536fe270137be3c24472
Róbert Selvek <robert.selvek@gmail.com>
2014-03-17 19:12:52 +0100

    Locally mostly playable

--- index.js
+++ index.js
@@ -15,15 +15,49 @@ $(function() {
 		var target = $(ev.target)
 		model[target.parent().data("row")][target.data("col")] = player;
 		render();
+		if (won(model, player)) {
+			alert("You won!");
+		}
+		$("#board").off("click", "td");
 	});
 
 	var render = function() {
 		$("#board").html(board({positions: model}));
 	}
 
-	var won = function() {
+	var won = function(board, player) {
+		var vertical = [[0, 0], [0, 1], [0, 2]];
+		var horizontal = [[0, 0], [1, 0], [2, 0]];
 
+		for (var i = 0; i < vertical.length; i++) {
+			var pos = vertical[i];
+			var result = (board[pos[0]][pos[1]] == player) && (board[pos[0]+1][pos[1]] == player) && (board[pos[0]+2][pos[1]] == player);
+			if (result) {
+				return true;
+			}
+		}
+
+		for (var i = 0; i < horizontal.length; i++) {
+			var pos = horizontal[i];
+			var result = (board[pos[0]][pos[1]] == player) && (board[pos[0]][pos[1] + 1] == player) && (board[pos[0]][pos[1] + 2] == player);
+			if (result) {
+				return true;
+			}
+		}
+
+		var result = (board[0][0] == player) && (board[1][1] == player) && (board[2][2] == player);
+		if (result) {
+			return true;
+		}
+		result = (board[0][2] == player) && (board[1][1] == player) && (board[2][0] == player);
+		if (result) {
+			return true;
+		}
+
+		return false;
 	}
 
 	render();
+
+	window.won = won;
 });
